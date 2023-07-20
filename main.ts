d3d9278a7fff5a6084c29908cfa776916e6c9100
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2023-07-20 15:48:01 +0100

    w1 content

--- main.ts
+++ main.ts
@@ -40,13 +40,22 @@ function spawn_horizontal () {
             dino.vy = speed
         } else {
             dino.top = 115
-            dino.vx = speed * -1
+            dino.vy = speed * -1
         }
         x += 40
     }
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (player_sprite, dino) {
-    game.gameOver(false)
+    info.changeLifeBy(-1)
+    timer.background(function () {
+        for (let index = 0; index < 3; index++) {
+            player1.setFlag(SpriteFlag.Invisible, true)
+            pause(300)
+            player1.setFlag(SpriteFlag.Invisible, false)
+            pause(300)
+        }
+    })
+    pause(2000)
 })
 function make_dino () {
     dino = sprites.create(assets.image`dino`, SpriteKind.Enemy)
@@ -59,17 +68,24 @@ function make_dino () {
     dino.setFlag(SpriteFlag.AutoDestroy, true)
     return dino
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeLifeBy(1)
+    sprites.destroy(otherSprite)
+})
 sprites.onDestroyed(SpriteKind.Enemy, function (sprite) {
     info.changeScoreBy(100)
 })
+let heart: Sprite = null
 let x = 0
 let dino: Sprite = null
 let y = 0
+let player1: Sprite = null
 let speed = 0
 speed = 75
-let player1 = sprites.create(assets.image`player1`, SpriteKind.Player)
+player1 = sprites.create(assets.image`player1`, SpriteKind.Player)
 controller.moveSprite(player1)
 scene.setBackgroundImage(assets.image`background`)
+info.setLife(3)
 game.onUpdate(function () {
     rumble()
 })
@@ -79,4 +95,9 @@ game.onUpdateInterval(5000, function () {
     } else {
         spawn_vertical()
     }
+})
+game.onUpdateInterval(10000, function () {
+    heart = sprites.create(assets.image`heart`, SpriteKind.Food)
+    heart.setPosition(randint(10, 150), randint(10, 110))
+    heart.lifespan = 5000
 })
